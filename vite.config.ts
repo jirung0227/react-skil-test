6d561881b8d59847a64a6e05d1a8bedd04f756ad
정지형 <gw03176370@gmail.com>
2024-07-10 22:03:03 +0900

    feat : 경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'; // @types/node가 설치되어 있어야 함
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    //경로 별칭 설정
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
+
+
