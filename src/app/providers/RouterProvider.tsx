d6c56583987eb69123bc206e2b800a36c4ace306
jirung0227 <gw03176370@gmail.com>
2024-07-11 23:40:00 +0900

    feat : error 페이지 추가

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -1,15 +1,44 @@
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Link,
+  RouterProvider,
+  useRouteError,
+} from 'react-router-dom';
+
+interface RouteError {
+  status?: string;
+  statusText?: string;
+  message?: string;
+  data?: string;
+}
+
+const ErrorPage = () => {
+  /**
+   * @description useRouteError훅은 현재 라우트의 에러를 반환합니다.
+   */
+  const error = useRouteError() as RouteError;
+  console.error(error);
+  /**
+   * @todo 400과 500에러 구분해서 다른 메시지 띄워주기
+   */
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">{error.status}</h1>
+      <p>{error.statusText || error.message}</p>
+      <p>{error.data}</p>
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    errorElement: <ErrorPage />,
+    path: '/',
     element: (
       <div>
         <h1>Hello World</h1>
         <Link to="about">About Us</Link>
-        <h1 className="text-3xl font-bold underline">
-          Hello world?
-        </h1>
+        <h1 className="text-3xl font-bold underline">Hello world?</h1>
       </div>
     ),
   },
