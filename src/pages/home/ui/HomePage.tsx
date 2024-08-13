1277b6a60703bcd48a11264a5b85522d87615afc
jirung0227 <gw03176370@gmail.com>
2024-08-13 23:54:59 +0900

    feat : content, footer 구조 정의

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,3 +1,8 @@
 export const HomePage = () => {
-  return <>홈</>;
+  return (
+    <div>
+      <section className="content h-[464px] bg-amber-300"></section>
+      <section className="banner max-w-[1200px] mx-auto">section</section>
+    </div>
+  );
 };
