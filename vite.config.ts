a79fb4d17721f45faf10c6a3114d3ee3f48fb255
jirung0227 <gw03176370@gmail.com>
2024-09-17 23:15:21 +0900

    feat : 백엔드 서버 요청 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 import path from 'path'; // @types/node가 설치되어 있어야 함
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +10,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
-
-
+  server: {
+    proxy: {
+      // '/api' 경로로 들어오는 요청을 프록시
+      '/api': {
+        target: 'http://localhost:3000', // 백엔드 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => {
+          const result = path.replace(/^\/api/, '');
+          return result;
+        },
+      },
+    },
+  },
+});
